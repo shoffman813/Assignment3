359873f5f1af50377492ae91cb2e00cd53aebcf7
root <shoffman2@crimson.ua.edu>
2019-04-09 22:55:24 -0500

    Adding readTrainingSet and findUserAverages

--- assignment3.cpp
+++ assignment3.cpp
@@ -5,3 +5,67 @@
 #include <cmath>
 using namespace std;
 
+/*Reads in the user data from file and saves to trainingSet vector*/
+void readTrainingSet(vector<vector<int> > &trainingSet) {
+
+	ifstream data;
+	data.open("dataset.txt");
+	
+	for(int i = 0; i < 200; i++) {
+		for(int j = 0; j < 1000; j++) {
+			data >> trainingSet.at(i).at(j);
+		}
+	}
+
+	data.close();
+	return;
+}
+
+/*Finds average rating of each user and stores to averageUserRating vector*/
+void findUserAverages(vector<vector<int> > trainingSet, vector<double> &averageUserRating) {
+	int sum = 0; //Sum of a row
+	double ratedMovies = 0; //Number of movies user actually rated (nonzero data values)
+
+	for(int i = 0; i < 200; i++) {
+		for(int j = 0; j < 1000; j++) {
+			sum += trainingSet.at(i).at(j); //Adding rating to sum
+			if(trainingSet.at(i).at(j) != 0) ratedMovies++; //User actually rated this movie
+		}
+		averageUserRating.at(i) = (double) sum / ratedMovies; //Diving sum by number of movies and adding avg to matrix
+		sum = 0;
+		ratedMovies = 0;
+	}
+
+	return;
+}
+
+/*Prints the trainingSet vector to a file for testing purposes*/
+void testMatrix(vector<vector<int> > trainingSet) {
+
+	ofstream myFile;
+	myFile.open("test.txt");
+	
+	for(int i = 0; i < 200; i++) {
+                for(int j = 0; j < 1000; j++) {
+			myFile << trainingSet.at(i).at(j) << "\t";
+                }
+		myFile << endl;
+        }
+	
+	myFile.close();
+	return;
+}
+
+int main() {
+	
+	/*Variable Declarations*/
+	vector<vector<int> > trainingSet(200, vector<int> (1000, 0));
+	vector<double> averageUserRating(200);
+
+	readTrainingSet(trainingSet); //Filling matrix with training set data
+
+	findUserAverages(trainingSet, averageUserRating); //Finds the average rating given by each user
+
+	return 0;
+}
+
